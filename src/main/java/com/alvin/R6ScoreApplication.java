d52e95c3f9b0b65cb34f74580e4afa4045045cc9
Alvin <ak47237496@gmail.com>
2020-08-18 23:56:56 +0800

    init

---
+++ src/main/java/com/alvin/R6ScoreApplication.java
@@ -0,0 +1,13 @@
+package com.alvin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R6ScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(R6ScoreApplication.class, args);
+    }
+
+}
